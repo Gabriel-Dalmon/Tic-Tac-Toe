ae559213a0f987b2ab74ee96f1b155195a8a7bd5
Gabriel Dalmon <dalmgabi@gmail.com>
2022-11-15 08:28:01 +0100

    Added displayBoard()

--- Game.py
+++ Game.py
@@ -5,7 +5,7 @@ import random
 class Game:
 
     def __init__(self,player1, player2):
-        self._board = [
+        self.board = [
             [0,0,0],
             [0,0,0],
             [0,0,0]
@@ -15,18 +15,17 @@ class Game:
         self.pTurn = random.randrange(2)
         self.isOver = False
 
-#=== Getters/Setters 
+#=== Display
 
-    def getBoard(self):
-        return self._board
-
+    def displayBoard(self):
+        print(self.board)
 
 #=== Conditions
     def isSpotExisting(self, spot):
         return True
 
     def isSpotFree(self, coords): #checks if the case is free to be taken by a player && coords contains two values (x,y)
-        if(self._board[coords[1]][coords[0]] == 0):
+        if(self.board[coords[1]][coords[0]] == 0):
             return True
 
 #=== Functions
@@ -37,23 +36,23 @@ class Game:
 
     def checkWinCond(self): #if Win Condition is verified, call setWinner function that also passes the self.isOver to True
         winCond = [3,-3] #the winCond is verified if the sum of a row, col, diag is equal to 3 or -3, it means every case in the row, col or diag is equal to either 1, either -1
-        lDiagSum = self._board[0][0] + self._board[1][1] + self._board[2][2] #Diagonal starting top left
-        rDiagSum = self._board[0][2] + self._board[1][1] + self._board[2][0] #Diagonal starting top right
+        lDiagSum = self.board[0][0] + self.board[1][1] + self.board[2][2] #Diagonal starting top left
+        rDiagSum = self.board[0][2] + self.board[1][1] + self.board[2][0] #Diagonal starting top right
         if(lDiagSum in winCond):
             self.setWinner(("diag",0))
         elif(rDiagSum in winCond):
             self.setWinner(("diag",1))
         else:
             for i in range(0,3):
-                rowSum = sum(self._board[i])
-                colSum = self._board[0][i] + self._board[1][i] + self._board[2][i]
+                rowSum = sum(self.board[i])
+                colSum = self.board[0][i] + self.board[1][i] + self.board[2][i]
                 if (rowSum in winCond):
                     self.setWinner(("row",i))
                 elif(colSum in winCond):
                     self.setWinner(("col",i))
 
     def updateSpot(self, coords): #update the board with the last spot picked and adapt to the actual player
-        self._board[coords[1]][coords[0]] = 1 + (self.pTurn * -2) #pTurn = à 1 ou 0, on différencie les joueurs sur le plateau avec 1 et -1
+        self.board[coords[1]][coords[0]] = 1 + (self.pTurn * -2) #pTurn = à 1 ou 0, on différencie les joueurs sur le plateau avec 1 et -1
 
     def playTurn(self):
         spot = self.players[self.pTurn].chooseSpot()
@@ -81,9 +80,8 @@ class Game:
 
 
 if __name__ == "__main__":
-    import Game
-    import Player
+    from Player import *
 
-    game = Game.Game(Player.Player('LnCol'), Player.Player('Json'))
+    game = Game(Player('LnCol'), Player('Json'))
 
     game.playGame()
--- Player.py
+++ Player.py
@@ -1,53 +1,14 @@
-from tkinter import *
-
 class Player:
 
     def __init__(self, name):
         self.name = name
-        self.initWindow()
         self.board = [
             [0,0,0],
             [0,0,0],
             [0,0,0]
         ]
 
-
     def chooseSpot(self):
         x = int(input("X: "))
         y = int(input("Y: "))
         return (x,y)
-
-    def initWindow(self):
-        self.window = Tk()
-        self.window.title("TicTacToe")
-        self.window.attributes ('-fullscreen', True)
-        headbar = Frame(self.window)
-        
-        self.gameGrid = Frame(self.window)
-        
-        delBtn = Button(headbar, text = 'X', fg="white", bg="red", bd=0, justify=CENTER, width = 5, relief = FLAT, command = self.window.destroy)
-        delBtn.pack(anchor="ne")
-
-        self.displayBoard()
-
-        headbar.pack(anchor="ne")
-        self.gameGrid.pack()
-
-    def drawCase(self, row, col, content):
-        btnTest = Button(self.gameGrid, text = content, fg="black", bg="white", width=15, height=15, justify=CENTER, relief = FLAT)
-        btnTest.grid(column=col+1, row=row+1, sticky=W, padx=5, pady=5)
-
-    def displayBoard(self):
-        displayedBoard = [
-            ["x","x","x"],
-            ["x","x","x"],
-            ["x","x","x"]
-        ]
-        for row in range (len(self._board)):
-            for col in range (len(self._board[row])):
-                if (self._board[row][col] == 1):
-                    displayedBoard[row][col] = "o"
-                elif (self._board[row][col] == -1):
-                    displayedBoard[row][col] = "x"
-                self.drawCase(row, col,displayedBoard[row][col])
-        print("",displayedBoard[0], "\n", displayedBoard[1], "\n", displayedBoard[2])
