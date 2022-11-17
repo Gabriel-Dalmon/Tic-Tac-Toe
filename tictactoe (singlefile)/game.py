1221c9b7e3a5e229f1523bce8e356da558ed67ee
Gabriel Dalmon <dalmgabi@gmail.com>
2022-11-17 09:18:54 +0100

    Added linesSum to game.py

--- tictactoe (singlefile)/game.py
+++ tictactoe (singlefile)/game.py
@@ -19,10 +19,34 @@ def consoleDisplay(board):
         displayBoard[2][0], "|",displayBoard[2][1], "|",displayBoard[2][2]    
     )
 
-def chooseSpot():
-    x = int(input("X: "))
-    y = int(input("Y: "))
+def chooseSpot(player):
+    if player == "CPU":
+
+        pass
+    else:
+        x = int(input("X: "))
+        y = int(input("Y: "))
     return (x,y)
+
+def linesSum(condition,board): #compares the sum of each lines with the parameter "condition", returns a list of each line
+
+    lDiagSum = board[0][0] + board[1][1] + board[2][2] #Diagonal starting top left
+    rDiagSum = board[0][2] + board[1][1] + board[2][0] #Diagonal starting top right
+    if(lDiagSum in condition):
+        return ("diag",0)
+    elif(rDiagSum in condition):
+        return ("diag",1)
+    else:
+        for i in range(0,3):
+            rowSum = sum(board[i])
+            colSum = board[0][i] + board[1][i] + board[2][i]
+            if (rowSum in condition):
+                return ("row",i)
+            elif(colSum in condition):
+                return ("col",i)
+    return None
+
+
 
 def updateSpot(coords, board, pTurn):
     board[coords[1]][coords[0]] = 1 + (pTurn * -2) #if opposer = 0 then case = 1 | if opposer = 1 then case = -1
@@ -53,8 +77,8 @@ def pTurnSwitch(pTurn):
     pTurn = 1 * (1 - pTurn) # if pTurn = 1 => then pTurn = 1*(1-1) = 0, if pTurn = 0 => then pTurn = 1*(1-0) = 1*1 = 1 || switches between 0 and 1
     return pTurn
 
-def playTurn(board, pTurn):
-    spot = chooseSpot()
+def playTurn(board, pTurn, player):
+    spot = chooseSpot(player)
 
     while (not isSpotFree(spot,board)): #if spot not free, we ask the player to pick a new one
         print("Spot is taken already, please choose another spot.")
@@ -76,7 +100,7 @@ def playGame():
     print(players[pTurn] + " starts the game.")
 
     while (not isOver):
-        playTurn(board, pTurn)
+        playTurn(board, pTurn, players[pTurn])
 
         isOver = isWinner(board)
         pTurn = pTurnSwitch(pTurn)
